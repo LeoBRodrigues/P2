907c771d46dfc14f035010509ea5df36f2e3864a
Leonardo <leonardo_r@fastmail.com>
2020-12-02 18:14:04 -0300

    implementação de passagem de dados, mascara de números.

--- lib/screen/screen1.dart
+++ lib/screen/screen1.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:p2_zanini/screen/screen2.dart';
 import 'package:p2_zanini/models/cliente.dart';
+import 'package:p2_zanini/services/api_handler.dart';
+
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
 
 class Screen1 extends StatelessWidget {
 
   final TextEditingController _controller = TextEditingController();
   Future<Cliente> _futureCliente;
+
+  var maskFormatter = new MaskTextInputFormatter(mask: '##.###.###/####-##', filter: { "#": RegExp(r'[0-9]') });
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +29,20 @@ class Screen1 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
+              inputFormatters: [ 
+                maskFormatter
+                ],
               controller: _controller,
               decoration: InputDecoration(hintText: 'Coloque o CNPJ')
             ),
             ElevatedButton(
               child: Text("Consulta de CNPJ"), 
               onPressed: (){
+                _controller.value = maskFormatter.updateMask(mask: '##############');
+                _futureCliente = fetchCliente(_controller.text);
                 Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Screen2()),
+                MaterialPageRoute(builder: (context) => Screen2(futureCliente: _futureCliente)),
               );
               }
             )]
--- lib/screen/screen2.dart
+++ lib/screen/screen2.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:p2_zanini/models/cliente.dart';
 
 class Screen2 extends StatelessWidget {
+
+  Future<Cliente> futureCliente;
+
+  Screen2({Key key, @required this.futureCliente}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Dados Cadastrados"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Encerrar consulta'),
+      body: 
+        Center(
+          child:
+            FutureBuilder<Cliente>(
+              future: futureCliente,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.nome);
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
+          
+                return CircularProgressIndicator();
+              },
+            )
         ),
-      ),
     );
   }
 }
